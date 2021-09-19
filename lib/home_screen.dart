6fb554be404bad1aa92be87b3bb96e96c555fa44
yasser-hoot <yasser@hoot.ae>
2021-09-19 15:27:03 +0400

    [IMP] black and white design

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -40,101 +40,156 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: Center(
-          child: FutureBuilder(
-            future: entries,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return generateList(snapshot.data);
-              }
-              if (snapshot.data == null) {
-                return Text('test');
-              }
-              return CircularProgressIndicator();
-            },
+      appBar: AppBar(
+        title: Text(
+          widget.title,
+          style: TextStyle(
+            color: Colors.black,
           ),
         ),
-        floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        elevation: 0,
+        bottom: PreferredSize(
+          child: Container(
+            color: Colors.grey,
+            height: 0.5,
+          ),
+          preferredSize: Size.fromHeight(0),
+        ),
+      ),
+      backgroundColor: Color(0xFFF0F0F0),
+      body: Center(
+        child: FutureBuilder(
+          future: entries,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return generateList(snapshot.data);
+            }
+            if (snapshot.data == null) {
+              return Text('Click on the Add button to create a new To-Do');
+            }
+            return CircularProgressIndicator();
+          },
+        ),
+      ),
+      bottomNavigationBar: Container(
+        padding: EdgeInsets.all(4.0),
+        height: 64,
+        decoration: BoxDecoration(
+          border: Border(
+            top: BorderSide(color: Colors.grey),
+          ),
+          color: Colors.white,
+        ),
+        child: ElevatedButton(
           onPressed: () {
             showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return AlertDialog(
-                    title: Text(
-                      'New Item',
-                      textAlign: TextAlign.center,
-                    ),
-                    content: SizedBox(
-                      height: 180,
-                      width: 100,
-                      child: Form(
-                        key: _formKey,
-                        child: Column(
-                          children: [
-                            TextFormField(
-                              autocorrect: true,
-                              textAlign: TextAlign.center,
-                              autofocus: true,
-                              keyboardType: TextInputType.multiline,
-                              maxLines: 4,
-                              controller: newItemController,
-                              validator: (value) {
-                                if (value == null || value.isEmpty) {
-                                  return 'Please enter some text or Cancel';
-                                }
-                                return null;
-                              },
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                ElevatedButton(
-                                  child: Text('Cancel'),
-                                  onPressed: () => Navigator.pop(context),
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  title: Text(
+                    'New Item',
+                    textAlign: TextAlign.center,
+                  ),
+                  content: SizedBox(
+                    height: 180,
+                    width: 100,
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          TextFormField(
+                            decoration: InputDecoration(
+                              focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Colors.black,
                                 ),
-                                ElevatedButton(
-                                  child: Text('Submit'),
-                                  onPressed: () {
-                                    if (_formKey.currentState!.validate()) {
-                                      databaseHelper.addItem(
-                                        TodoItem(
-                                            id: 0,
-                                            title: newItemController.text,
-                                            subTitle: '',
-                                            isDone: false),
-                                      );
-                                      getEntries();
-                                      return Navigator.pop(context);
-                                    }
-                                  },
-                                ),
-                              ],
+                              ),
                             ),
-                          ],
-                        ),
+                            cursorColor: Colors.grey,
+                            autocorrect: true,
+                            textAlign: TextAlign.center,
+                            autofocus: true,
+                            keyboardType: TextInputType.multiline,
+                            maxLines: 4,
+                            controller: newItemController,
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter some text or Cancel';
+                              }
+                              return null;
+                            },
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              ElevatedButton(
+                                child: Text(
+                                  'Cancel',
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                  ),
+                                ),
+                                style: ElevatedButton.styleFrom(
+                                  elevation: 0.0,
+                                  primary: Colors.white,
+                                ),
+                                onPressed: () {
+                                  newItemController.clear();
+                                  return Navigator.pop(context);
+                                },
+                              ),
+                              ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  elevation: 0.0,
+                                  primary: Colors.white,
+                                ),
+                                child: Text(
+                                  'Submit',
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                  ),
+                                ),
+                                onPressed: () {
+                                  if (_formKey.currentState!.validate()) {
+                                    databaseHelper.addItem(
+                                      TodoItem(
+                                          id: 0,
+                                          title: newItemController.text,
+                                          subTitle: '',
+                                          isDone: false),
+                                    );
+                                    getEntries();
+                                    newItemController.clear();
+                                    return Navigator.pop(context);
+                                  }
+                                },
+                              ),
+                            ],
+                          ),
+                        ],
                       ),
                     ),
-                  );
-                });
-            // databaseHelper.addItem(
-            //   TodoItem(
-            //     id: 0,
-            //     title: 'title',
-            //     subTitle: 'subTitle',
-            //     isDone: false,
-            //   ),
-            // );
-            // getEntries();
+                  ),
+                );
+              },
+            );
           },
-          tooltip: 'Increment',
-          child: Icon(Icons.add),
-        ));
+          child: Icon(Icons.add, color: Colors.black),
+          style: ElevatedButton.styleFrom(
+            shape: CircleBorder(
+                side: BorderSide(
+              color: Colors.grey,
+              width: 1.5,
+            )),
+            primary: Colors.white,
+            elevation: 0.0,
+          ),
+        ),
+      ),
+    );
   }
 
   ListView generateList(data) {
@@ -144,10 +199,13 @@ class _HomeScreenState extends State<HomeScreen> {
       itemBuilder: (BuildContext context, int index) {
         TodoItem item = data.elementAt(index);
         return Container(
+          margin: EdgeInsets.fromLTRB(0, 0, 0, 8),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10.0),
+          ),
           child: CheckboxListTile(
-            title: Text('Entry ${item.title}'),
-            subtitle: Text('this is the subtitle'),
-            secondary: const Icon(Icons.alarm),
+            title: Text('${item.title}'),
             value: item.isDone,
             onChanged: (value) {
               item.isDone = value!;
